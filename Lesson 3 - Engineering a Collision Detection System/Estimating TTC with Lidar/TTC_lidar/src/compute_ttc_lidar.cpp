f991e9228bb41801a10f2944b25597734bf62c22
josper <hcv1027@gmail.com>
2020-12-29 14:48:53 +0800

    Complete Exercise

--- Lesson 3 - Engineering a Collision Detection System/Estimating TTC with Lidar/TTC_lidar/src/compute_ttc_lidar.cpp
+++ Lesson 3 - Engineering a Collision Detection System/Estimating TTC with Lidar/TTC_lidar/src/compute_ttc_lidar.cpp
@@ -10,17 +10,22 @@ using namespace std;
 void computeTTCLidar(std::vector<LidarPoint> &lidarPointsPrev,
                      std::vector<LidarPoint> &lidarPointsCurr, double &TTC) {
   // auxiliary variables
-  double dT = 0.1;         // time between two measurements in seconds
-  double laneWidth = 4.0;  // assumed width of the ego lane
+  constexpr double dT = 0.1;         // time between two measurements in seconds
+  constexpr double laneWidth = 4.0;  // assumed width of the ego lane
+  constexpr double halfLaneWidth = laneWidth / 2;
 
   // find closest distance to Lidar points within ego lane
   double minXPrev = 1e9, minXCurr = 1e9;
   for (auto it = lidarPointsPrev.begin(); it != lidarPointsPrev.end(); ++it) {
-    minXPrev = minXPrev > it->x ? it->x : minXPrev;
+    if (std::fabs(it->y) <= halfLaneWidth) {
+      minXPrev = minXPrev > it->x ? it->x : minXPrev;
+    }
   }
 
   for (auto it = lidarPointsCurr.begin(); it != lidarPointsCurr.end(); ++it) {
-    minXCurr = minXCurr > it->x ? it->x : minXCurr;
+    if (std::fabs(it->y) <= halfLaneWidth) {
+      minXCurr = minXCurr > it->x ? it->x : minXCurr;
+    }
   }
 
   // compute TTC from both measurements
